cf4855b33f7ba47ba2eebcd7859a791e908a0b7c
domjoeymoncada <domjoeymoncada@gmail.com>
2023-08-04 10:36:30 +0800

    Add homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,18 @@
-import { Row, Col, Form, Input, Button, Typography } from "antd";
-import { useDispatch } from "react-redux";
-import { addMovieRecommendation } from "./redux/features/movieListSlice";
+import { Button, Typography } from "antd";
+import { Link } from "react-router-dom";
 
-const { TextArea } = Input;
 const { Title } = Typography;
 
 const App = () => {
-  const dispatch = useDispatch();
-
-  const onFormSubmit = (values) => {
-    dispatch(addMovieRecommendation(values));
-  };
-
   return (
-    <Form name="recommendation-form" layout="vertical" onFinish={onFormSubmit}>
-      <Row align="middle" justify="space-around">
-        <Col span={18}>
-          <div>
-            <Row>
-              <Col span={8}>
-                <Form.Item
-                  name="title"
-                  label={<Title level={4}>Movie title</Title>}
-                >
-                  <Input placeholder="Movie title..." />
-                </Form.Item>
-              </Col>
-            </Row>
-            <Row>
-              <Col span={12}>
-                <Form.Item
-                  name="description"
-                  label={<Title level={3}>Description</Title>}
-                >
-                  <TextArea
-                    showCount
-                    maxLength={500}
-                    placeholder="Write a short description"
-                    style={{ height: 200 }}
-                  />
-                </Form.Item>
-              </Col>
-            </Row>
-            <Button htmlType="submit" style={{ marginTop: 18 }}>
-              Send recommendation
-            </Button>
-          </div>
-        </Col>
-      </Row>
-    </Form>
+    <div>
+      <Title level={2}>Redux Toolkit (RTK) sample application</Title>
+      <Button style={{ marginTop: 24 }}>
+        <Link to={{ pathname: "/send-movie-recommendation-form" }}>
+          Click here to start
+        </Link>
+      </Button>
+    </div>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -4,19 +4,13 @@ import { store } from "./redux/store.ts";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MovieRecommendationForm from "./pages/PageA/index.tsx";
 import DefaultActions from "./pages/PageB/index.tsx";
+import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              <h1>Welcome to home screen</h1>
-            </div>
-          }
-        />
+        <Route path="/" element={<App />} />
         <Route
           path="/send-movie-recommendation-form"
           element={<MovieRecommendationForm />}
